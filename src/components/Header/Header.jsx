8a265107e57f32b72319da11f626c928e7df670e
gustavfaustino <gustav.faustino107@gmail.com>
2024-11-18 01:53:24 -0300

    Revert "Refactor Header component for improved styling and navigation; added smooth scrolling for section links and updated CSS for better layout and responsiveness."
    This reverts commit 20c4b5bd485e490c9944e70a0be943a1d024ae7c.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,9 @@
 import React from "react";
 import Logo from "../../assets/miniLogo.svg";
 import "./Header.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  const scrollToSection = (id) => {
-    const section = document.getElementById(id);
-    if (section) {
-      section.scrollIntoView({ behavior: "smooth", block: "start" });
-    }
-  };
-
   return (
     <header id="header-section">
       <img src={Logo} alt="Logo da Leva Nóis" />
