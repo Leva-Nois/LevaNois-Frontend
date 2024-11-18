7577cf52f034ae4f75fac7cf923d343975fe2401
Felipe Dias <felipediaslim13@gmail.com>
2024-11-18 15:48:59 -0300

    adicionei a tela de cadastro ao "app.js"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Home from "./pages/Home/LandingPage";
+import Cadastro from "./pages/cadastro/Cadastro.jsx";
 import { Routes, Route } from "react-router-dom"; // Importa o Routes e o Route do react-router-dom
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="/login" element={ <Cadastro />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </div>
